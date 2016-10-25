01b0e311bc64d72bd23ce9ad5c05c9945c6f3108
morning <morning@thecover.cn>
2016-10-25 18:12:20 +0800

    first commit

--- NewlySwiftDemo/ViewController.swift
+++ NewlySwiftDemo/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireObjectMapper
+import ObjectMapper
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NetworkWrapper.defaultWrapper.Get(parameters: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,41 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+
+
+let networkUrl = "http://localhost:3000/hello"
+//let header = ["":""]
+let header: [String: String]? = nil
+class NetworkWrapper: NSObject {
+    
+    
+    static let defaultWrapper = NetworkWrapper()
+    
+    private override init() {
+    }
+    
+    
+    func Get(parameters: Parameters?) {
+        Alamofire.request(networkUrl, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: header).responseObject { (response: DataResponse<BaseResponse>?) in
+            print(response?.debugDescription)
+            let user = response?.result.value?.user
+            print("user:" + "\(user)")
+        }
+    }
+}
+
+class BaseResponse: Mappable {
+    required init?(map: Map) {
+        
+    }
+    
+    var user: String?
+    
+    func mapping(map: Map) {
+        user <- map["user"]
+    }
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'NewlySwiftDemo' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NewlySwiftDemo
+  pod 'Alamofire', '~> 4.0.1'
+  pod 'AlamofireObjectMapper', '~> 4.0.0'
+
+  target 'NewlySwiftDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'NewlySwiftDemoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
